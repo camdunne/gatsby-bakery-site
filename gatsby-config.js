10578a66b076fcba5bfe25aca4db745817e9a6fb
Cameron <camdunne@gmail.com>
2020-07-04 11:28:54 -0400

    fill config for cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bakery Shop`,
+    description: `Basic site to sell goods.`,
+    author: `@camdunne`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`, 
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        enableIdentityWidget: false,
+      }
+    }
   ],
 }
